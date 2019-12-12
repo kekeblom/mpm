a7d314428a6e4770177e56a6a1bc4e40dca91e05
sduenser <simon.duenser@inf.ethz.ch>
2019-12-12 11:14:08 +0100

    more scenes

--- src/mpm.cpp
+++ src/mpm.cpp
@@ -399,8 +399,79 @@ int main(int argc, char *argv[]) {
              ParticleType,
              InterpolationKernel> simulation(flags, InterpolationKernel());
 
+  // "Scene selector"
+  // usage: uncomment the desired scene below ;)
+  // (also, set the correct path to the mesh directory)
+  std::string meshes_dir = "../meshes/";
+
   ////////////////////////////
-  // Examples
+  // Scene: Snowman gets hit (by snowball)
+  ////////////////////////////
+  simulation.addObject(meshes_dir + "sphere.obj",
+                       MaterialModel(1.0/flags.particle_count, 700, 1.4e5, 0.2, 10, 1.0-2.5e-2, 1.0+0.75e-2),  // soft, light snow
+                       0.4,
+                       Vec(0.3, 0.03, 0.3),
+                       Vec(0.0,0.0,0.0));
+  simulation.addObject(meshes_dir + "sphere.obj",
+                       MaterialModel(1.0/flags.particle_count, 700, 1.4e5, 0.2, 10, 1.0-2.5e-2, 1.0+0.75e-2),  // soft, light snow
+                       0.3,
+                       Vec(0.35, 0.43, 0.35),
+                       Vec(0.0,0.0,0.0));
+  simulation.addObject(meshes_dir + "sphere.obj",
+                       MaterialModel(1.0/flags.particle_count, 700, 1.4e5, 0.2, 10, 1.0-2.5e-2, 1.0+0.75e-2),  // soft, light snow
+                       0.2,
+                       Vec(0.4, 0.75, 0.4),
+                       Vec(0.0,0.0,0.0));
+  simulation.addObject(meshes_dir + "sphere.obj",
+                       MaterialModel(1.0/flags.particle_count, 500, 1.4e5, 0.2, 10, 1.0-1.5e-2, 1.0+0.5e-2),  // soft, light snow
+                       0.1,
+                       Vec(0.05, 0.5, 0.46),
+                       Vec(7.0,2.5,0.0),
+                       0.5);
+
+  ////////////////////////////
+  // Scene: Rubber duck hets hit (by metal cube)
+  ////////////////////////////
+//  simulation.addObject(meshes_dir + "rubber_duck.obj",
+//                       MaterialModel(1.0/flags.particle_count, 200, 1.4e5, 0.2, 0, 0.0, 1.0e30),
+//                       0.4,
+//                       Vec(0.3, 0.4, 0.3),
+//                       Vec(0.0,0.0,0.0));
+//  simulation.addObject(meshes_dir + "cube.obj",
+//                       MaterialModel(1.0/flags.particle_count, 8000, 5e7, 0.45, 0, 0.0, 1.0e30), // "Fluid"????
+//                       0.3,
+//                       Vec(0.35, 0.5, 0.35),
+//                       Vec(0.0,0.0,0.0),
+//                       2.0);
+
+  ////////////////////////////
+  // Scene: Liquid bunny
+  ////////////////////////////
+//  simulation.addObject(meshes_dir + "stanford_bunny.obj",
+//                       MaterialModel(1.0/flags.particle_count, 1000, 1.4e5, 0.45, 0, 0.975, 0.975),
+//                       0.5,
+//                       Vec(0.25, 0.4, 0.25),
+//                       Vec(0.0,0.0,0.0),
+//                       0.0);
+
+  ////////////////////////////
+  // Scene: Marshmallow duck and water
+  ////////////////////////////
+//  simulation.addObject(meshes_dir + "rubber_duck.obj",
+//                       MaterialModel(1.0/flags.particle_count, 200, 1.4e5, 0.2, 0, 0.0, 1.0e30),
+//                       0.4,
+//                       Vec(0.3, 0.4, 0.3),
+//                       Vec(0.0,0.0,0.0));
+//  simulation.addObject(meshes_dir + "cube.obj",
+//                       MaterialModel(1.0/flags.particle_count, 1000, 1.4e5, 0.45, 0, 0.975, 0.975), // "Fluid"????
+//                       0.35,
+//                       Vec(0.325, 0.5, 0.325),
+//                       Vec(0.0,0.0,0.0),
+//                       1.8);
+
+
+  ////////////////////////////
+  // General Examples with mesh from command line arguments
   ////////////////////////////
 //  simulation.addObject(flags.load_mesh,
 //                       MaterialModel(1.0/flags.particle_count, 400, 1.4e5, 0.2, 10, 1.0-1e-2, 1.0+3e-3),  // soft, light snow
@@ -425,45 +496,6 @@ int main(int argc, char *argv[]) {
 //                       0.5,
 //                       Vec(0.25, 0.4, 0.25),
 //                       Vec(0.0,0.0,0.0));
-
-  ////////////////////////////
-  // Scene: Snowman gets hit
-  ////////////////////////////
-
-  std::string meshes_dir = "../meshes/";
-  simulation.addObject(meshes_dir + "sphere.obj",
-                       MaterialModel(1.0/flags.particle_count, 700, 1.4e5, 0.2, 10, 1.0-2.5e-2, 1.0+0.75e-2),  // soft, light snow
-                       0.4,
-                       Vec(0.3, 0.03, 0.3),
-                       Vec(0.0,0.0,0.0));
-  simulation.addObject(meshes_dir + "sphere.obj",
-                       MaterialModel(1.0/flags.particle_count, 700, 1.4e5, 0.2, 10, 1.0-2.5e-2, 1.0+0.75e-2),  // soft, light snow
-                       0.3,
-                       Vec(0.35, 0.43, 0.35),
-                       Vec(0.0,0.0,0.0));
-  simulation.addObject(meshes_dir + "sphere.obj",
-                       MaterialModel(1.0/flags.particle_count, 700, 1.4e5, 0.2, 10, 1.0-2.5e-2, 1.0+0.75e-2),  // soft, light snow
-                       0.2,
-                       Vec(0.4, 0.75, 0.4),
-                       Vec(0.0,0.0,0.0));
-
-  simulation.addObject(meshes_dir + "sphere.obj",
-                       MaterialModel(1.0/flags.particle_count, 500, 1.4e5, 0.2, 10, 1.0-1.5e-2, 1.0+0.5e-2),  // soft, light snow
-                       0.1,
-                       Vec(0.05, 0.5, 0.46),
-                       Vec(7.0,2.5,0.0),
-                       0.5);
-
-  ////////////////////////////
-  // Scene: Rubber Duck
-  ////////////////////////////
-//  std::string meshes_dir = "../meshes/";
-//  simulation.addObject(meshes_dir + "rubber_duck.obj",
-//                       MaterialModel(1.0/flags.particle_count, 100, 2.0e5, 0.2, 0, 0.0, 1.0e30),
-//                       0.4,
-//                       Vec(0.3, 0.4, 0.3),
-//                       Vec(0.0,0.0,0.0));
-
 
 
   ParticleWriter writer;
